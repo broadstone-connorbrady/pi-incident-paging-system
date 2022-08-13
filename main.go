8a4af03b5ff5852cb997de90c08024e750fce162
broadstone-connorbrady <connor.brady@broadstone-engage.com>
2022-08-13 22:02:52 +0100

    ADDED Binary (This is only for debugging)

--- main.go
+++ main.go
@@ -1,2 +1,25 @@
 package main
 
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"os/exec"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.POST("/opsgenie/alert-created", func(c *gin.Context) {
+		cmd := exec.Command("sh", "-c", "echo -e \"1923929:Front door buzzer activated\" | ./pocsag -f \"138075000\" -b 3 -r 1200")
+
+		err := cmd.Start()
+		fmt.Println("The command is running")
+		if err != nil {
+			fmt.Println(err)
+		}
+	})
+
+	// echo -e "1923929:Front door buzzer activated" | sudo ./pocsag -f "138075000" -b 3 -r 1200
+
+	r.Run()
+}
